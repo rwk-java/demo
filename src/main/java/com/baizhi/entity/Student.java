e30ea8489b8435ef0e7ddea5bf725f5b88cb2bfd
rwk <1315669405@qq.com>
2020-01-03 18:52:14 +0800

    v2.0

---
+++ src/main/java/com/baizhi/entity/Student.java
@@ -0,0 +1,18 @@
+package com.baizhi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student implements Serializable {
+    private String id;
+    private String name;
+    private Integer age;
+    private String sex;
+    private Date birthday;
+}
