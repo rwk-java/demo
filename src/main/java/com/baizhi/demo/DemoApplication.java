5aff241a4ff1a0fa7cb3fbd2c67b2050d08d959c
rwk-java <58070402+rwk-java@users.noreply.github.com>
2020-01-03 14:30:30 +0800

    Update DemoApplication.java

--- src/main/java/com/baizhi/demo/DemoApplication.java
+++ src/main/java/com/baizhi/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("你好");
         System.out.println("hello");
+        System.out.println("我也好");
     }
 
 }
